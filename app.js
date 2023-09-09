97b8f858016cb13f3961b5a99c7e4942960d145e
Emir Pirija <118456820+EmirPirija@users.noreply.github.com>
2023-09-09 22:32:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+const reset = document.querySelector(".btn-reset");
+
+let result = 0;
+let hitPosition;
+let currentTime = 3;
+let timerId = null;
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
+}
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id === hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 600);
+}
+
+moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime === 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("GAME OVER! Your final score is " + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
+
+reset.addEventListener("click", function () {
+  randomSquare();
+  moveMole();
+  resetGame();
+});
+
+function resetGame() {
+  score.textContent = "0";
+  result = 0;
+}
